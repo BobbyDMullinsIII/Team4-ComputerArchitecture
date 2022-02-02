55371aa43ecae2fd8cb518963d3632ec1828f8df
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2022-02-02 16:58:23 -0500

    Cleaned up UI and simplified parts of code

--- Team4-Project1/Team4-Project1/MainForm.Designer.cs
+++ Team4-Project1/Team4-Project1/MainForm.Designer.cs
@@ -28,91 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-			this.textInput = new System.Windows.Forms.TextBox();
-			this.textOutput = new System.Windows.Forms.TextBox();
-			this.decodeButton = new System.Windows.Forms.Button();
-			this.encodeButton = new System.Windows.Forms.Button();
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-			this.toolstripFile = new System.Windows.Forms.ToolStripDropDownButton();
-			this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.encodedLabel = new System.Windows.Forms.Label();
-			this.decodedLabel = new System.Windows.Forms.Label();
-			this.toolStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// textInput
-			// 
-			resources.ApplyResources(this.textInput, "textInput");
-			this.textInput.Name = "textInput";
-			// 
-			// textOutput
-			// 
-			resources.ApplyResources(this.textOutput, "textOutput");
-			this.textOutput.Name = "textOutput";
-			this.textOutput.ReadOnly = true;
-			// 
-			// decodeButton
-			// 
-			resources.ApplyResources(this.decodeButton, "decodeButton");
-			this.decodeButton.Name = "decodeButton";
-			this.decodeButton.UseVisualStyleBackColor = true;
-			// 
-			// encodeButton
-			// 
-			resources.ApplyResources(this.encodeButton, "encodeButton");
-			this.encodeButton.Name = "encodeButton";
-			this.encodeButton.UseVisualStyleBackColor = true;
-			// 
-			// toolStrip1
-			// 
-			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
-			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.disassembleButton = new System.Windows.Forms.Button();
+            this.assembleButton = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolstripFile = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.machineCodeLabel = new System.Windows.Forms.Label();
+            this.assemblyLanguageLabel = new System.Windows.Forms.Label();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.textInput, "textInput");
+            this.textInput.Name = "textInput";
+            // 
+            // textOutput
+            // 
+            this.textOutput.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.textOutput, "textOutput");
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ReadOnly = true;
+            // 
+            // disassembleButton
+            // 
+            this.disassembleButton.BackColor = System.Drawing.Color.DimGray;
+            resources.ApplyResources(this.disassembleButton, "disassembleButton");
+            this.disassembleButton.ForeColor = System.Drawing.Color.Black;
+            this.disassembleButton.Name = "disassembleButton";
+            this.disassembleButton.UseVisualStyleBackColor = false;
+            this.disassembleButton.Click += new System.EventHandler(this.disassembleButton_Click);
+            // 
+            // assembleButton
+            // 
+            this.assembleButton.BackColor = System.Drawing.Color.DimGray;
+            resources.ApplyResources(this.assembleButton, "assembleButton");
+            this.assembleButton.ForeColor = System.Drawing.Color.Black;
+            this.assembleButton.Name = "assembleButton";
+            this.assembleButton.UseVisualStyleBackColor = false;
+            this.assembleButton.Click += new System.EventHandler(this.assembleButton_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolstripFile});
-			resources.ApplyResources(this.toolStrip1, "toolStrip1");
-			this.toolStrip1.Name = "toolStrip1";
-			// 
-			// toolstripFile
-			// 
-			this.toolstripFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-			this.toolstripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openFileToolStripMenuItem});
-			resources.ApplyResources(this.toolstripFile, "toolstripFile");
-			this.toolstripFile.Name = "toolstripFile";
-			// 
-			// openFileToolStripMenuItem
-			// 
-			this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
-			resources.ApplyResources(this.openFileToolStripMenuItem, "openFileToolStripMenuItem");
-			this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
-			// 
-			// encodedLabel
-			// 
-			resources.ApplyResources(this.encodedLabel, "encodedLabel");
-			this.encodedLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-			this.encodedLabel.Name = "encodedLabel";
-			// 
-			// decodedLabel
-			// 
-			resources.ApplyResources(this.decodedLabel, "decodedLabel");
-			this.decodedLabel.Name = "decodedLabel";
-			// 
-			// MainForm
-			// 
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.decodedLabel);
-			this.Controls.Add(this.encodedLabel);
-			this.Controls.Add(this.toolStrip1);
-			this.Controls.Add(this.encodeButton);
-			this.Controls.Add(this.decodeButton);
-			this.Controls.Add(this.textOutput);
-			this.Controls.Add(this.textInput);
-			this.Name = "MainForm";
-			this.toolStrip1.ResumeLayout(false);
-			this.toolStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // toolstripFile
+            // 
+            this.toolstripFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolstripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.instructionsToolStripMenuItem});
+            resources.ApplyResources(this.toolstripFile, "toolstripFile");
+            this.toolstripFile.Name = "toolstripFile";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            resources.ApplyResources(this.openFileToolStripMenuItem, "openFileToolStripMenuItem");
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // machineCodeLabel
+            // 
+            resources.ApplyResources(this.machineCodeLabel, "machineCodeLabel");
+            this.machineCodeLabel.BackColor = System.Drawing.Color.Black;
+            this.machineCodeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.machineCodeLabel.ForeColor = System.Drawing.Color.White;
+            this.machineCodeLabel.Name = "machineCodeLabel";
+            // 
+            // assemblyLanguageLabel
+            // 
+            resources.ApplyResources(this.assemblyLanguageLabel, "assemblyLanguageLabel");
+            this.assemblyLanguageLabel.BackColor = System.Drawing.Color.Black;
+            this.assemblyLanguageLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.assemblyLanguageLabel.ForeColor = System.Drawing.Color.White;
+            this.assemblyLanguageLabel.Name = "assemblyLanguageLabel";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            resources.ApplyResources(this.instructionsToolStripMenuItem, "instructionsToolStripMenuItem");
+            this.instructionsToolStripMenuItem.Click += new System.EventHandler(this.instructionsToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.Controls.Add(this.assemblyLanguageLabel);
+            this.Controls.Add(this.machineCodeLabel);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.assembleButton);
+            this.Controls.Add(this.disassembleButton);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.textInput);
+            this.Name = "MainForm";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -120,13 +142,14 @@
 
 		private System.Windows.Forms.TextBox textInput;
 		private System.Windows.Forms.TextBox textOutput;
-		private System.Windows.Forms.Button decodeButton;
-		private System.Windows.Forms.Button encodeButton;
+		private System.Windows.Forms.Button disassembleButton;
+		private System.Windows.Forms.Button assembleButton;
 		private System.Windows.Forms.ToolStrip toolStrip1;
 		private System.Windows.Forms.ToolStripDropDownButton toolstripFile;
 		private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
-		private System.Windows.Forms.Label encodedLabel;
-		private System.Windows.Forms.Label decodedLabel;
-	}
+		private System.Windows.Forms.Label machineCodeLabel;
+		private System.Windows.Forms.Label assemblyLanguageLabel;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+    }
 }
 
